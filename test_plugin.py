89366f8f910ac600fb747ee344cf2e4cf0f5e89f
T_wangyaxiong <2523682567@qq.com>
2020-10-09 18:05:50 +0800

    add test_plugin

--- test_plugin.py
+++ test_plugin.py
@@ -2,4 +2,45 @@ import pytest
 
 
 def test_div():
-    assert 1 == 2
+    1/0
+
+def test_pytest_assume():
+    # assert 1 == 1
+    # assert 1 == 2
+    # assert 2 == 2
+    pytest.assume(1 == 1)
+    pytest.assume(1 == 2)
+    pytest.assume(2 == 2)
+    print("执行完成")
+
+@pytest.mark.run(order=3)
+def test_ordering1():
+    print("ordering1")
+
+@pytest.mark.run(order=1)
+def test_ordering2():
+    print("ordering2")
+
+@pytest.mark.run(order=2)
+def test_ordering3():
+    print("ordering3")
+
+@pytest.mark.dependency()
+def test_caseA():
+    print("test_caseA")
+    raise Exception
+
+
+@pytest.mark.dependency(depends=['test_caseA'])
+def test_caseB():
+    print("test_caseB")
+
+@pytest.mark.dependency(name='caseA')
+def test_caseA():
+    print("test_caseA")
+    raise Exception
+
+
+@pytest.mark.dependency(depends=['caseA'])
+def test_caseB():
+    print("test_caseB")
